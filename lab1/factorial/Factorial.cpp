e3c06091d5ad97c8e20169fb2dbb9f4ec13be1f3
marganski <domestos234@wp.pl>
2018-03-02 17:29:38 +0100

    solves Factorial Problem

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,14 @@
 #include "Factorial.h"
 
 int factorial(int value) {
+  if(value<13 && value>-13) {
+    if (value == 1 || value == 0) {
+      return 1;
+    }
+    if (value < 0) {
+      return factorial(value + 1) * value;
+    }
+    return factorial(value - 1) * value;
+  }
   return 0;
 }
